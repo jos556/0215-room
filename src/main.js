ae729ce1b7d1f1813359cb379907ef5fdc9d30b4
LAPTOP-9IRE96SJ\88690 <d25823095@gmail.com>
2023-03-07 13:13:04 +0800

    0307

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import '../style.css'
 import App from './App.vue'
+import"bootstrap/dist/css/bootstrap.min.css";
+import"bootstrap"
 import Experience from '../experience/experience'
 
 
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 })
+
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [vue({ template: {
