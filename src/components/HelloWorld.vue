ab2868df25bf13a41a1780b39e42082be494d972
LAPTOP-9IRE96SJ\88690 <d25823095@gmail.com>
2023-03-14 16:46:17 +0800

    0314

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,129 @@
 <script setup>
-import { ref } from 'vue'
 
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <Particles
+      id="tsparticles"
+      :particlesInit="particlesInit"
+      :particlesLoaded="particlesLoaded"
+      :options="options"
+    />
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
+<script setup lang='ts'>
+import { reactive } from 'vue'
+import { loadFull } from "tsparticles"
+import type { Engine } from 'tsparticles-engine'
+
+const particlesInit = async (engine:Engine) => {
+  await loadFull(engine)
+}
+
+const particlesLoaded = async (container:any) => {
+  console.log('Particles container loaded', container)
+}
+const options = reactive({
+    background: {
+      color: {
+        value: '#000000' // 粒子颜色
+      }
+    },
+    fpsLimit: 60,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: 'bubble' // 可用的click模式有: "push", "remove", "repulse", "bubble"。
+        },
+        onHover: {
+          enable: true,
+          mode: 'repulse' // 可用的hover模式有: "grab", "repulse", "bubble"。
+        },
+        resize: true
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.6,
+          size: 40
+        },
+        push: {
+          quantity: 4
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4
+        }
+      }
+    },
+    particles: {
+      color: {
+        value: '#d99898'
+      },
+      links: {
+        color: '#ffffff', // '#dedede'。线条颜色。
+        distance: 150, // 线条长度
+        enable: false, // 是否有线条
+        opacity: 0.5, // 线条透明度。
+        width: 1 // 线条宽度。
+      },
+      collisions: {
+        enable: false
+      },
+      move: {
+        direction: 'down',
+        enable: true,
+        outMode: 'out',
+        random: false,
+        speed: 6, // 粒子运动速度。
+        straight: false
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800
+        },
+        value: 80 // 粒子数量。
+      },
+      opacity: {
+        value: 0.5 // 粒子透明度。
+      },
+      shape: {
+        type: 'edge' // 可用的粒子外观类型有："circle","edge","triangle", "polygon","star"
+      },
+      size: {
+        random: true,
+        value: 8
+      }
+    },
+    detectRetina: true
+  })
+
+</script>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
 import '../style.css'
-import App from './App.vue'
 import"bootstrap/dist/css/bootstrap.min.css";
 import"bootstrap"
 import Experience from '../experience/experience'
+import "./main1"
 
 
 import {} from './main.ce.js'
 
-const experience = new Experience(document.querySelector(".experience-canvas"))
+const experience = new Experience(document.querySelector(".experience-canvas"));
+
+
