680852285a32bc78e1aa2176582ed1e5135bcaaf
LAPTOP-9IRE96SJ\88690 <d25823095@gmail.com>
2024-09-01 15:52:15 +0800

    updated

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,7 +4,7 @@
             <li><div class="aa" style="display: flex;"><a href="/">Home</a></div></li>
             <li><div class="aa" style="display: flex;"><a href="mailto:d25823095@gmail.com?subject=聯繫郵件&body=這是一封聯繫給兪帆的郵件。">Email</a></div></li>
             <li><div class="aa" style="display: flex;"><a href="https://9rise.com/" target="_blank" rel="noopener noreferrer">九日生</a></div></li>
-            <li><div class="aa" style="display: flex;"><a href="../dist/assets/履歷.pdf" target="_blank">查看履歷</a></div></li>
+            <li><div class="aa" style="display: flex;"><a href="履歷.pdf" target="_blank">查看履歷</a></div></li>
             <li><div class="aa" style="display: flex;"><a href="tel:0905286032">打電話給兪帆</a></div></li>
         </ul>
         <div class="copyright"><p class="l-footer__main-bottom__copyright u-ff-poppins">2023 All Rights Reserved</p></div>
